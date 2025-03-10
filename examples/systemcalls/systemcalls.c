f73357fdd9413a1ec9f50a3a70bb687fdcb4a982
Nues0913 <a09871530274@gmail.com>
2025-03-10 17:58:02 +0800

    f1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include "systemcalls.h"
 
 /**
@@ -16,8 +22,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret = system(cmd);
 
-    return true;
+    if(ret == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
@@ -47,7 +58,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 /*
  * TODO:
@@ -58,6 +69,27 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+
+    if (command[0][0] != '/') {
+        return false;
+    }
+
+    pid_t pid = fork();
+    if(pid == -1) {
+        return false;
+    } else if (pid == 0) {
+        if(execv(command[0], command) == -1) {
+            return false;
+        }
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     va_end(args);
 
@@ -82,7 +114,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
 
 /*
@@ -92,8 +124,41 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    pid_t pid = fork();
+    if (pid == -1) {
+        return false;
+    } else if (pid == 0) {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (fd == -1) {
+            return false;
+        }
+        if (dup2(fd, STDOUT_FILENO) == -1) {
+            close(fd);
+            return false;
+        }
+        close(fd);
+        if (strcmp(command[0], "/bin/sh") == 0) {
+            if (execv(command[0], command) == -1) {
+                return false;
+            }
+        } else {
+            if (execv(command[0], command) == -1) {
+                return false;
+            }
+        }
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     va_end(args);
 
     return true;
 }
+
