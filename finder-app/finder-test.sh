b0ad2b837106fed7757320990b1d261bc22d012f
nues0913 <a09871530274@gmail.com>
2025-03-29 21:07:03 +0800

    fix finder-test.sh, makefile

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,11 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+WRITEFILE=/tmp/assignment4-result.txt
+username=$(cat /etc/finder-app/conf/username.txt)
+THISDIR=`dirname $0`
+cd $THISDIR
+echo "start script at $THISDIR"
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +36,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -53,10 +57,11 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR" > $WRITEFILE
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+echo $OUTPUTSTRING >> $WRITEFILE
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
